947fd3b2667cd791f2686b8bcdcffe62b85606b1
seandmac <sdm7306@uncw.edu>
2015-11-04 21:51:49 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {background: #B0C4DE;}
+h1   {color:blue}
+p    {color:green}
+</style>
+</head>
+<body>
+
+<div id="main">
+<div class="container-fluid">
+<center>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h1>
+					Medi-Buddy
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<form class="form-horizontal" role="form">
+				<div class="form-group">
+					 
+					<label for="inputEmail3" class="col-sm-2 control-label">
+						Enter UserID
+					</label>
+					<div class="col-sm-10">
+						<input type="UserID" class="form-control" id="inputEmail3" />
+					</div>
+				</div>
+				
+				
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						 
+						
+						<FORM  ACTION="modulemenu.php">
+							
+						</FORM>
+
+						<form action="modulemenu.php">
+						<button>Participant</button>
+						</form>
+
+						<form action="adminscreen.php">
+						<button>Administrator</button>
+						</form>
+
+
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+<center>
+</div>
+</div>
+
+</body>
+</html>
+
