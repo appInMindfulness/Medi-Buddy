b78ec804de8d2161ed0fb7e8f2338d00c100c439
seandmac <sdm7306@uncw.edu>
2015-11-04 21:49:23 -0500

    Create index.php
    This is the controller

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include('view/header.php'); ?>
+
+
+
+
+<div id="main">
+	<div id="section">
+		<h1>Welcome to Medi-Buddy</h1></p>
+		<h3>An Application in Mindfulness</h3>
+		<p>Here, you will learn to practice mindfulness!<br>
+		Our project is to create a web-based user experience where the user<br>
+		 is taught Buddhist mindfulness techniques through a series of guided meditation modules.<br> 
+		These modules will teach the user about different aspects of meditation such as<br>
+		 breathing, concentration, and focus. The user will be prompted throughout to ensure understanding of the techniques explored.</p>
+		<p>Please <a href="model/login.php">Login</a> to begin your mindfulness experience.</p>
+
+	</div>
+</div>
+
+
+
+
+<?php include('view/footer.php'); ?>
